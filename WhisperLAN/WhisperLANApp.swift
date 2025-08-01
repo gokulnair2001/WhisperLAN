82c30d47e7463bda46e95a0f2a22cad80336d3e4
gokulnair2001 <gokul.nair@gomotive.com>
2025-08-01 18:33:13 +0530

    Initial Commit

---
+++ WhisperLAN/WhisperLANApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperLANApp.swift
+//  WhisperLAN
+//
+//  Created by Gokul Nair on 01/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisperLANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
